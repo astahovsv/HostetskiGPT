786d89fcdd5d60ef044f679488b652587f8a4cb3
Sergey Astakhov <astahovsv@gmail.com>
2024-07-20 20:24:32 +0500

    update

--- Http/Controllers/HostetskiGPTController.php
+++ Http/Controllers/HostetskiGPTController.php
@@ -91,10 +91,10 @@ class HostetskiGPTController extends Controller
 
         $gptThread = $client->threads()->create([]);
         
-        // $client->threads()->messages()->create($gptThread->id, [
-        //     'role' => 'user',
-        //     'content' => $request->get('query'),
-        // ]);
+        $client->threads()->messages()->create($gptThread->id, [
+            'role' => 'user',
+            'content' => $request->get('query'),
+        ]);
 
         // $client->threads()->runs()->create(
         //     threadId: $gptThread->id, 
@@ -103,13 +103,25 @@ class HostetskiGPTController extends Controller
         //     ],
         // );
 
-        // $gptMessages = $client->threads()->messages()->list($gptThread->id, [
-        //     'limit' => 1,
-        // ]);
+        $stream = $client->threads()->runs()->createStreamed(
+            threadId: $gptThread->id,
+            parameters: [
+                'assistant_id' => 'asst_1SBgvdStUpzj9GobOmeRkixd',
+            ],
+        );
+        
+        foreach($stream as $response) {
+            $a = $response->event;
+            $b = $response->response;
+        };
+
+        $gptMessages = $client->threads()->messages()->list($gptThread->id, [
+            'limit' => 10,
+        ]);
 
         // $gptMessage = $gptMessages->data[0];
         // $gptAnswer = $gptMessage->content->text->value;
-        $gptAnswer = json_encode($gptThread, JSON_UNESCAPED_UNICODE);
+        $gptAnswer = json_encode($gptMessages, JSON_UNESCAPED_UNICODE);
         
         $thread = Thread::find($request->get('thread_id'));
         $answers = [];
