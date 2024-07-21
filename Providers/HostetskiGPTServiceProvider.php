9817f2ec6ea2a191e4ceed61c1a037e06d71f769
Sergey Astakhov <astahovsv@gmail.com>
2024-07-21 20:52:40 +0500

    update

--- Providers/HostetskiGPTServiceProvider.php
+++ Providers/HostetskiGPTServiceProvider.php
@@ -102,7 +102,7 @@ class HostetskiGPTServiceProvider extends ServiceProvider
                             </a>
                         </div>
                     <?php endforeach ?>
-                    <div 
+                    <button 
                         data-thread-id="<?php echo $thread->id ?>" 
                         data-message-index="<?php echo $index ?>" 
                         class="gpt-message-toolbar-item gpt-message-copy"
@@ -110,7 +110,7 @@ class HostetskiGPTServiceProvider extends ServiceProvider
                         <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#000000">
                             <path d="M360-240q-29.7 0-50.85-21.15Q288-282.3 288-312v-480q0-29.7 21.15-50.85Q330.3-864 360-864h384q29.7 0 50.85 21.15Q816-821.7 816-792v480q0 29.7-21.15 50.85Q773.7-240 744-240H360Zm0-72h384v-480H360v480ZM216-96q-29.7 0-50.85-21.15Q144-138.3 144-168v-552h72v552h456v72H216Zm144-216v-480 480Z"/>
                         </svg>
-                    </div>
+                    </button>
                 </div>
                 <?php foreach ($messages as $index => $message): ?>
                     <div class="alert alert-note gpt-message-text gpt-message-<?php echo $index ?> <?php if ($index != $current_index): ?>hidden<?php endif ?>">
